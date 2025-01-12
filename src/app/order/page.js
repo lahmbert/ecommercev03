0f30b8862808429b44b72bb92a95439f94c92bf0
Muhammad Sabran <muhammadsabran86.com>
2025-01-13 00:42:59 +0800

    Code

--- src/app/order/page.js
+++ src/app/order/page.js
@@ -113,7 +113,7 @@ const OrderPage = () => {
     });
 
     const encodedMessage = encodeURIComponent(message);
-    const phoneNumber = '085334679379'; // Ganti dengan nomor WhatsApp yang menerima pesan
+    const phoneNumber = '+6285334679379'; // Ganti dengan nomor WhatsApp yang menerima pesan
     return `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
   };
 
